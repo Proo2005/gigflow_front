ba1aaa770f694ea7be337334075543c13dd7035d
Proo2005 <prochak1922@gmail.com>
2026-01-14 23:36:16 +0530

    footer added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -130,7 +130,7 @@ export default function Navbar() {
 
               <button
                 onClick={() => {
-                  router.push("/findfreelancers");
+                  router.push("/freelancerjob");
                   setOpenSearch(false);
                 }}
                 className="block w-full text-left px-4 py-2 text-gray-300 hover:bg-[#121212]"
